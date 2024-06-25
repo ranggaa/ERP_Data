fe0d7acd7f9a309a154c1e2a7b7ad753d2b228e9
lalatendu11 <lalatendung@gmail.com>
2024-06-25 20:24:27 +0530

    New Class Created

---
+++ src/test/java/com/pages/Demo.java
@@ -0,0 +1,28 @@
+package com.pages;
+
+public class Demo {
+public void add()
+{
+	int a=23;
+	int b=45;
+	int c=a+b;
+	System.out.println(c);
+	
+}
+public void div()
+{
+	int a=67;
+	int b=6;
+	int c=a+b;
+	System.out.println(c);
+	
+}
+public void mul()
+{
+	int a=67;
+	int b=7;
+	int c=a+b;
+	System.out.println(c);
+	
+}
+}
